7268da349e244a2597c1716d25bcd9bfdb8fa5e2
amante-8917 <amante.8917@gmail.com>
2025-11-09 23:56:12 +0530

    Implementing search functionality with list options from api

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,22 @@ const Home = () => {
 
   const {allCoin,currency}=useContext(CoinContext);
   const [displayCoin,setDisplayCoin]=useState([]);
+  const [input, setInput] = useState('');
+  
+  const inputHandler=(e)=>{
+    setInput(e.target.value);
+    if(e.target.value===""){
+      setDisplayCoin(allCoin);
+    }
+  }
+
+  const searchHandler=async(e)=>{
+    e.preventDefault();
+    const coins=await allCoin.filter((item)=>{
+      return item.name.toLowerCase().includes(input.toLowerCase())
+    })
+    setDisplayCoin(coins);
+  }
 
   useEffect(() => {
     setDisplayCoin(allCoin);
@@ -17,8 +33,15 @@ const Home = () => {
       <div className="hero">
         <h1>Largest<br/>Crypto Marketplace</h1>
         <p>Welcome to the world's largest cryptocurrency marketplace.Sign up to explore more about cryptos.</p>
-        <form>
-          <input type="text" placeholder='Search crypto...' />
+        <form onSubmit={searchHandler}>
+
+
+          <input onChange={inputHandler} list='coinlist' value={input} type="text" placeholder='Search crypto...' required />
+
+          <datalist id='coinlist'>
+            {allCoin.map((item,ind)=>(<option key={ind} value={item.name}/>))}
+          </datalist>
+
           <button type='submit'>Search</button>
         </form>
       </div>
@@ -39,10 +62,14 @@ const Home = () => {
                 <p>{item.name+" - "+item.symbol}</p>
               </div>
               <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
+
               <p className={item.price_change_percentage_24h>0?"green":"red"}>
-                ^↑↓▲▼▽△
-                {Math.floor(item.price_change_percentage_24h*100)/100}</p>
-              <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}</p>
+                
+                {item.price_change_percentage_24h>0?'△':'▽'}
+                {Math.floor(item.price_change_percentage_24h*100)/100}
+              </p>
+              <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}
+              </p>
             </div>
           ))
         }
